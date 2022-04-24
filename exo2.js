b635e4df66d9de2109ec9d44ca85e307d1c24cea
rindrajosia <rrindrajosia@gmail.com>
2022-04-24 16:00:59 +0300

    Works with variables and constantes

---
+++ exo2.js
@@ -0,0 +1,9 @@
+/*****************************
+** Assigner le bon nom
+** importance: 3
+* Créez la variable avec le nom de notre planète. Comment nommeriez-vous une telle variable ?
+* Créez la variable pour stocker le nom du visiteur actuel. Comment nommeriez-vous cette variable ?
+*/
+
+let notrePlanete = "Terre";
+let currentUserName = "Josia";
